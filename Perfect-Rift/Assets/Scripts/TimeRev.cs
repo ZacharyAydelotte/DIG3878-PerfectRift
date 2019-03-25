9cdba7f641dbbee2f6d83d426bf83cb1159523d8
YLAKidJT <46692627+YLAKidJT@users.noreply.github.com>
2019-03-25 12:50:55 -0400

    Added Rewind Mechanic

---
+++ Perfect-Rift/Assets/Scripts/TimeRev.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeRev : MonoBehaviour
+{
+    public GameObject indic;
+    public float rewindLength = 5.0f;
+    bool isRewinding = false;
+    List<PointInTime> pointsInTime;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsInTime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            indic.SetActive(true);
+            StartRewind();
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            indic.SetActive(false);
+            StopRewind();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isRewinding)
+            Rewind();
+        else
+            Record();
+    }
+
+    void Rewind()
+    {
+        if (pointsInTime.Count > 0)
+        {
+            PointInTime pointInTime = pointsInTime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsInTime.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind();
+        }
+    }
+
+    void Record()
+    {
+        if (pointsInTime.Count > Mathf.Round(rewindLength / Time.fixedDeltaTime))
+        {
+            pointsInTime.RemoveAt(pointsInTime.Count - 1);
+        }
+
+        pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        rb.isKinematic = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;
+    }
+}
