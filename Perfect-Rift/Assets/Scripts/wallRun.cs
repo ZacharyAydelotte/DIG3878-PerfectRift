0e8b69ac99e07e1b60770223166790ac7b243d2d
SoherSilva <47155577+SoherSilva@users.noreply.github.com>
2019-03-30 00:59:18 -0400

    Level 2 assets added

---
+++ Perfect-Rift/Assets/Scripts/wallRun.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class wallRun : MonoBehaviour
+{
+    private bool isWallR = false;
+    private bool isWallL = false;
+    private RaycastHit hitR;
+    private RaycastHit hitL;
+    private int jumpCount = 0;
+    private RigidbodyFirstPersonController cc;
+    private Rigidbody rb;
+    public float runTime = 0.5f;
+
+    void Start()
+    {
+        cc = GetComponent<RigidbodyFirstPersonController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (cc.Grounded)
+        {
+            jumpCount = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && !cc.Grounded && jumpCount <= 1)
+        {
+            if (Physics.Raycast(transform.position, -transform.right, out hitL, 1))
+            {
+                if (hitL.transform.tag == "Wall")
+                {
+                    isWallL = true;
+                    isWallR = false;
+                    jumpCount += 1;
+                    rb.useGravity = false;
+                    StartCoroutine(afterRun());
+                }
+            }
+            if (Physics.Raycast(transform.position, transform.right, out hitR, 1))
+            {
+                if (hitR.transform.tag == "Wall")
+                {
+                    isWallR = true;
+                    isWallL = false;
+                    jumpCount += 1;
+                    rb.useGravity = false;
+                    StartCoroutine(afterRun());
+                }
+            }
+
+        }
+    }
+    IEnumerator afterRun()
+    {
+        yield return new WaitForSeconds(runTime);
+        isWallL = false;
+        isWallR = false;
+        rb.useGravity = true;
+    }
+}
