8cbe97f59881886e957c87d55f5541cc18f14607
SoherSilva <47155577+SoherSilva@users.noreply.github.com>
2019-03-31 00:40:03 -0400

    Level 2 modified

--- Perfect-Rift/Assets/Scripts/Teleport.cs
+++ Perfect-Rift/Assets/Scripts/Teleport.cs
@@ -9,7 +9,7 @@ public class Teleport : MonoBehaviour
     {
         if (myCollider.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(00);
+            SceneManager.LoadScene(01);
         }
     }
 }
