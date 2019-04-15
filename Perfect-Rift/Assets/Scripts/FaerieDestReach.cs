f664eebf49748b93a189b4ed0f8da13fa78f6bb9
Justine Tan <ylakidjt@ufl.edu>
2019-04-15 07:00:32 -0400

    Faerie Update
    Added PathFinding Faerie - Bound to 'Q'

---
+++ Perfect-Rift/Assets/Scripts/FaerieDestReach.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaerieDestReach : MonoBehaviour
+{
+    public GameObject faerie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Faerie")
+        {
+            faerie.SetActive(false);
+            Debug.Log("touch");
+        }
+    }
+}
