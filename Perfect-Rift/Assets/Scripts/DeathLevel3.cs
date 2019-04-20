3f2f7879c23ce24d096e866c9efa283562929c7e
SoherSilva <47155577+SoherSilva@users.noreply.github.com>
2019-04-20 10:28:02 -0400

    Level 3 assets added. Path under construction

---
+++ Perfect-Rift/Assets/Scripts/DeathLevel3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathLevel3 : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (gameObject.transform.position.y < -300)
+        {
+            SceneManager.LoadScene(sceneName: "Level3");
+        }
+    }
+}
