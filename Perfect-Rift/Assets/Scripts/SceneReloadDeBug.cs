d6cdaed6b71492a3f40f24c23af826015e3100ef
Justine Tan <ylakidjt@ufl.edu>
2019-04-21 16:28:12 -0400

    Fixed Cursor Lock & Added Runtime Debug

---
+++ Perfect-Rift/Assets/Scripts/SceneReloadDeBug.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReloadDeBug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R) && Input.GetKey(KeyCode.E) && Input.GetKey(KeyCode.U) && Input.GetKeyDown(KeyCode.P))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+        if (Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.O) && Input.GetKey(KeyCode.R) && Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject.Find("RigidBodyFPSController").GetComponent<TimeRev>().rwCounter += 5;
+        }
+    }
+}
